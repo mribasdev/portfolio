0b5556d0b9fdebf4dedbb1267747f8f795ec82e0
Moisés Ribas <mribasdev@gmail.com>
2024-08-23 14:26:09 -0300

    teste

--- scrollbar.js
+++ scrollbar.js
@@ -3,22 +3,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const dots = document.querySelectorAll('.scroll-dot');
   
     window.addEventListener('scroll', function () {
-        let current = '';
-        sections.forEach((section, index) => {
-            const sectionTop = section.offsetTop;
-            const sectionHeight = section.offsetHeight; // Alterado de clientHeight para offsetHeight
+      let current = '';
+      sections.forEach((section, index) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
   
-            // Atualizado para levar em conta o deslocamento do scroll
-            if (pageYOffset >= sectionTop - (sectionHeight / 3) && pageYOffset < (sectionTop + sectionHeight)) {
-                current = section.getAttribute('id');
-            }
-        });
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+          current = section.getAttribute('id') || index + 1;
+        }
+      });
   
-        dots.forEach(dot => {
-            dot.classList.remove('active');
-            if (dot.dataset.section === current) {
-                dot.classList.add('active');
-            }
-        });
+      dots.forEach(dot => {
+        dot.classList.remove('active');
+        if (dot.dataset.section == current) {
+          dot.classList.add('active');
+        }
+      });
     });
-});
+  });
+  
